17db6e10f25c8d53b06b98b3738ebe7b60c5fde5
Prapya <dev.prapya@gmail.com>
2026-01-28 05:04:03 -0500

    typescript issue in dashboard overview component fixed

--- pages/DashboardOverview.jsx
+++ pages/DashboardOverview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+// dashboard overview
 const DashboardOverview = () => {
   const blogs = useSelector((state) => state.blogs.blogs);
   const { currentUser, isAuthenticated } = useSelector((state) => state.user);
